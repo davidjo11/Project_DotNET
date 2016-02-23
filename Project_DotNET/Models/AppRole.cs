3f9a239452ff719666f783cad5dab980265f5742
edgd1er <edgd1er@hotmail.com>
2016-02-23 11:35:47 +0100

    add AppRole (application role, related to user) and available role (list of role tu be used by user)

---
+++ Project_DotNET/Models/AppRole.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project_DotNET.Models
+{
+    public class AppRole
+    {
+        
+        public AppRole()
+        {
+            this.AppRoles = new List<AvailableRole>();
+        }
+        public int AppRoleId { get; set; }
+
+        public string AppRoleName { get; set; }
+
+        public string AppRoleDesc { get; set; }
+
+        public virtual ICollection<AvailableRole> AppRoles { get; set; }
+
+        public bool addRole(AvailableRole Role)
+        {
+            var size = this.AppRoles.Count;
+            this.AppRoles.Add(Role);
+            if (size + 1 == this.AppRoles.Count)
+            {
+                this.AppRoles.OrderBy(x => x.AvailableRoleName);
+                return true;
+            }
+            return false;
+        }
+    }
+
+}
---
+++ Project_DotNET/Models/AvailableRole.cs
@@ -0,0 +1,15 @@
+﻿namespace Project_DotNET.Models
+{
+    public class AvailableRole
+    {
+
+        //[Column(Order = 3)]
+        //Key
+        public int AvailableRoleId { get; set; }
+
+        public string AvailableRoleName { get; set; }
+
+        public string AvailableRoleDesc { get; set; }
+
+    }
+}
